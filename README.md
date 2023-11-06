c0838909e5c13aeb37d8a1890612b965e525ea4a
mishellguanoc <ygirl12abb@gmail.com>
2023-11-05 21:06:38 -0500

    PARTE 5: Limpieza y preparacion de datos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-El proyecto de este curso consiste en analizar el conjunto de datos introducido en esta sección, procesarlo, limpiarlo y finalmente ajustar modelos de machine learning para realizar predicciones sobre estos datos.
+El proyecto de este curso consiste en analizar el conjunto de datos introducido en esta sección, procesarlo, 
+limpiarlo y finalmente ajustar modelos de machine learning para realizar predicciones sobre estos datos.
--- solution.py
+++ solution.py
@@ -1,5 +1,5 @@
-from datasets import load_dataset
 import numpy as np
+import pandas as pd
 import requests
 import os
 
@@ -16,11 +16,48 @@ def descargar_datos(url, nombre_archivo):
     else:
         print(f'Error al descargar los datos. Código de estado: {response.status_code}')
 
+
 # URL de los datos
 url_datos = 'https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv' 
 
 # Nombre del archivo CSV en el que se guardarán los datos
-archivo_csv = 'datos.csv'
+archivo_txt = 'respuesta.txt'
 
 # Llama a la función para descargar y guardar los datos
-descargar_datos(url_datos, archivo_csv)
+descargar_datos(url_datos, archivo_txt)
+
+def limpiar_y_preparar_datos(dataframe):
+    # Verificar que no existan valores faltantes
+    if dataframe.isnull().values.any():
+        dataframe.dropna(inplace=True)
+        print("Valores faltantes eliminados.")
+    # Verificar que no existan filas repetidas
+    if dataframe.duplicated().any():
+        dataframe.drop_duplicates(inplace=True)
+        print("Filas duplicadas eliminadas.")
+    # Verificar si existen valores atipicos y eliminarlos
+    Q1 = dataframe['age'].quantile(0.25)
+    Q3 = dataframe['age'].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    dataframe = dataframe[(dataframe['age'] >= lower_bound) & (dataframe['age'] <= upper_bound)]
+    print("Valores atípicos en la columna 'age' eliminados.")
+    # Crear una columna que categorice por edades
+    dataframe['edad_categorizada'] = pd.cut(
+        dataframe['age'],
+        bins=[0, 12, 19, 39, 59, np.inf],
+        labels=['Niño', 'Adolescente', 'Jóvenes adulto', 'Adulto', 'Adulto mayor']
+    )
+
+    # Guardar el resultado como csv
+    dataframe.to_csv('datos_limpios.csv', index=False)
+    print("Datos limpios guardados como 'datos_limpios.csv'.")
+
+
+# Cargar los datos desde el archivo CSV descargado
+archivo_csv = 'respuesta.txt'
+dataframe = pd.read_csv(archivo_csv)
+
+# Llamar a la función de limpieza y preparación de datos
+limpiar_y_preparar_datos(dataframe)
